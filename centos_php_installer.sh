d9ed9b837e028ac05b26a06d20660ed480bb56fe
@小小只^v^ <littlezov@qq.com>
2021-06-12 23:18:32 +0800

    fix

--- centos_php_installer.sh
+++ centos_php_installer.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-PHP_VERSION=8.0
+PHP_VERSION=7.4
 # 初始化
 rpmkeys --import file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial &&
-  INSTALL_PKGS="bsdtar \
+    INSTALL_PKGS="bsdtar \
   findutils \
   gettext \
   groff-base \
@@ -42,21 +42,21 @@ rpmkeys --import file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial &&
   openssl \
   mysql-devel \
   zlib-devel" &&
-  mkdir -p "${HOME}"/.pki/nssdb &&
-  chown -R 1001:0 "${HOME}"/.pki &&
-  yum install -y --setopt=tsflags=nodocs "$INSTALL_PKGS" &&
-  rpm -V "$INSTALL_PKGS" &&
-  rm -rf /var/cache/yum/* &&
-  yum repolist &&
-  yum -y clean all --enablerepo='*' &&
-  dnf install dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm -y && yum -y upgrade --setopt=tsflags=nodocs --nogpgcheck &&
-  rm -rf /var/cache/yum/* &&
-  yum repolist &&
-  yum -y clean all --enablerepo='*'
+    mkdir -p "${HOME}"/.pki/nssdb &&
+    chown -R 1001:0 "${HOME}"/.pki &&
+    yum install -y --setopt=tsflags=nodocs "$INSTALL_PKGS" &&
+    rpm -V "$INSTALL_PKGS" &&
+    rm -rf /var/cache/yum/* &&
+    yum repolist &&
+    yum -y clean all --enablerepo='*' &&
+    dnf install dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm -y && yum -y upgrade --setopt=tsflags=nodocs --nogpgcheck &&
+    rm -rf /var/cache/yum/* &&
+    yum repolist &&
+    yum -y clean all --enablerepo='*'
 # 安装 环境
 dnf module reset php &&
-  dnf module enable php:remi-${PHP_VERSION} -y &&
-  dnf install pkgconfig -y && INSTALL_PKGS="php-gd php-xhprof php-ast php-cli php-dba php-dbg php-pdo \
+    dnf module enable php:remi-${PHP_VERSION} -y &&
+    dnf install pkgconfig -y && INSTALL_PKGS="php-gd php-xhprof php-ast php-cli php-dba php-dbg php-pdo \
   php-xml php-imap php-intl php-json php-ldap php-snmp php-soap \
   php-tidy php-devel php-bcmath php-brotli php-common php-recode \
   php-sodium php-xmlrpc php-enchant php-libvirt php-mysqlnd \
@@ -72,17 +72,17 @@ dnf module reset php &&
   php-pecl-igbinary php-pecl-mogilefs php-pecl-json-post \
   php-pecl-ip2location php-pecl-http-message php-gmp php-pecl-apcu \
   php-zip php-swoole" &&
-  yum install -y libstdc++ openssl pcre-devel pcre2-devel openssl-devel supervisor \
-    "$INSTALL_PKGS" --skip-broken --setopt=tsflags=nodocs --nogpgcheck && rm -rf /var/cache/yum/* &&
-  yum repolist &&
-  yum -y clean all --enablerepo='*' &&
-  wget -O /usr/local/bin/composer https://mirrors.aliyun.com/composer/composer.phar &&
-  chmod a+x /usr/local/bin/composer &&
-  composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/ && wget -O /usr/local/bin/php-cs-fixer https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.18.6/php-cs-fixer.phar &&
-  chmod a+x /usr/local/bin/php-cs-fixer &&
-  sed -i "s@;date.timezone =@date.timezone = Asia/Shanghai@g" /etc/php.ini &&
-  sed -i "s@max_execution_time = 30@max_execution_time = 300@g" /etc/php.ini &&
-  sed -i "s@post_max_size = 8M@post_max_size = 32M@g" /etc/php.ini &&
-  sed -i "s@max_input_time = 60@max_input_time = 600@g" /etc/php.ini &&
-  sed -i "s@memory_limit = 128M@memory_limit = 2048M@g" /etc/php.ini &&
-  sed -i "2i swoole.use_shortname=off\nswoole.unixsock_buffer_size=32M" /etc/php.d/40-swoole.ini
+    yum install -y libstdc++ openssl pcre-devel pcre2-devel openssl-devel supervisor \
+        "$INSTALL_PKGS" --skip-broken --setopt=tsflags=nodocs --nogpgcheck && rm -rf /var/cache/yum/* &&
+    yum repolist &&
+    yum -y clean all --enablerepo='*' &&
+    wget -O /usr/local/bin/composer https://mirrors.aliyun.com/composer/composer.phar &&
+    chmod a+x /usr/local/bin/composer &&
+    composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/ && wget -O /usr/local/bin/php-cs-fixer https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.18.6/php-cs-fixer.phar &&
+    chmod a+x /usr/local/bin/php-cs-fixer &&
+    sed -i "s@;date.timezone =@date.timezone = Asia/Shanghai@g" /etc/php.ini &&
+    sed -i "s@max_execution_time = 30@max_execution_time = 300@g" /etc/php.ini &&
+    sed -i "s@post_max_size = 8M@post_max_size = 32M@g" /etc/php.ini &&
+    sed -i "s@max_input_time = 60@max_input_time = 600@g" /etc/php.ini &&
+    sed -i "s@memory_limit = 128M@memory_limit = 2048M@g" /etc/php.ini &&
+    sed -i "2i swoole.use_shortname=off\nswoole.unixsock_buffer_size=32M" /etc/php.d/40-swoole.ini
